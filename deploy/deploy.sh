f6ce47935eefc154d45abd46b51140511b60b0f3
Tim Schwecke <tim_schwecke@hotmail.com>
2013-06-11 22:33:31 -0600

    Modifying deployment script to deploy to prod

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-ncftpput -f deploy/ftp.config /devsite1/volunteer .htaccess
-ncftpput -f deploy/ftp.config /devsite1/volunteer services.php
-ncftpput -f deploy/ftp.config /devsite1/volunteer Volunteer.html
-ncftpput -f deploy/ftp.config /devsite1/volunteer config.ini
-ncftpput -R -f deploy/ftp.config /devsite1/volunteer Controllers
-ncftpput -R -f deploy/ftp.config /devsite1/volunteer Domain
-ncftpput -R -f deploy/ftp.config /devsite1/volunteer Middleware
-ncftpput -R -f deploy/ftp.config /devsite1/volunteer Models
-ncftpput -R -f deploy/ftp.config /devsite1/volunteer scripts
-ncftpput -R -f deploy/ftp.config /devsite1/volunteer Slim
-ncftpput -R -f deploy/ftp.config /devsite1/volunteer styles
-ncftpput -R -f deploy/ftp.config /devsite1/volunteer Util
+if [ -z "$1" ];
+then
+	echo "Missing the environment.  You must pass either 'dev' or 'prod' as the first argument."
+	exit -1
+fi
+
+if [ "$1" == "prod" ];
+then
+	DEPLOYMENT_PATH="/site/volunteer"
+else
+	DEPLOYMENT_PATH="/devsite1/volunteer"
+fi
+
+ncftpput -f deploy/ftp.config $DEPLOYMENT_PATH .htaccess
+ncftpput -f deploy/ftp.config $DEPLOYMENT_PATH services.php
+ncftpput -f deploy/ftp.config $DEPLOYMENT_PATH Volunteer.html
+ncftpput -f deploy/ftp.config $DEPLOYMENT_PATH config.ini
+ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH Controllers
+ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH Domain
+ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH Middleware
+ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH Models
+ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH scripts
+ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH Slim
+ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH styles
+ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH Util
 
