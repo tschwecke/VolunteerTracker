cb7808d1d86b2728877d872ab2f855a012e0e5ae
Tim Schwecke <tim_schwecke@hotmail.com>
2014-08-18 12:17:16 -0600

    refactoring deployment scripts

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,26 +2,21 @@
 
 if [ -z "$1" ];
 then
-	echo "Missing the environment.  You must pass either 'dev' or 'prod' as the first argument."
-	exit -1
+  echo "Missing the environment.  You must pass either 'dev' or 'prod' as the first argument."
+  exit -1
 fi
 
 if [ "$1" == "prod" ];
 then
-	DEPLOYMENT_PATH="/volunteer"
+  DEPLOYMENT_PATH="/volunteer"
 else
-	DEPLOYMENT_PATH="/devsite1/volunteer"
+  DEPLOYMENT_PATH="/devsite1/volunteer"
 fi
 
-ncftpput -f deploy/ftp.config $DEPLOYMENT_PATH .htaccess
-ncftpput -f deploy/ftp.config $DEPLOYMENT_PATH services.php
+
 ncftpput -f deploy/ftp.config $DEPLOYMENT_PATH Volunteer.html
-ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH Controllers
-ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH Domain
-ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH Middleware
-ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH Models
+ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH admin
+ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH kiosk
 ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH scripts
-ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH Slim
 ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH styles
-ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH Util
 
