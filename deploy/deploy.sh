34f2abefff9a033a72c3d37cabf0075c5baeefd6
Tim Schwecke <tim_schwecke@hotmail.com>
2017-08-20 23:14:30 -0600

    Not sure why, but the changes to the deploy script didn't work, so I'm changing it back

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if [ -z "$1" ];
+then
+  echo "Missing the environment.  You must pass either 'dev' or 'prod' as the first argument."
+  exit -1
+fi
+
+if [ "$1" == "prod" ];
+then
+  DEPLOYMENT_PATH="/"
+else
+  DEPLOYMENT_PATH="/devsite1/volunteer"
+fi
+
 
 ncftpput -f deploy/ftp.config $DEPLOYMENT_PATH Volunteer.html
 ncftpput -R -f deploy/ftp.config $DEPLOYMENT_PATH admin
